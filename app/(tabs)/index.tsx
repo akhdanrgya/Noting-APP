0f9a2a3d24e35d717f1fe541571cd2e4d781b8bb
akhdanrgya <akhdan.anargya@gmail.com>
2024-11-04 17:14:27 +0700

    First

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,197 @@
+import React from 'react'
+import { View, Text, StyleSheet, TextInput, ScrollView, TouchableOpacity, Image } from 'react-native'
+import { FontAwesome } from '@expo/vector-icons'
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.greeting}>Hi, Akhdan 👋</Text>
+        <Image
+          source={{ uri: 'https://via.placeholder.com/50' }}
+          style={styles.profileImage}
+        />
+      </View>
+
+      <View style={[styles.searchContainer, styles.shadowSearch]}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Cari artikel, video, atau berita"
+        />
+        <FontAwesome name="search" size={18} color="#888888" style={styles.searchIcon} />
+      </View>
+
+      <View style={styles.categoryContainer}>
+        <Text style={styles.sectionTitle}>Artikel populer</Text>
+        <TouchableOpacity>
+          <Text style={styles.viewAll}>Lihat semua</Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.chipContainer}>
+        <TouchableOpacity style={styles.chip}>
+          <Text style={styles.chipText}>Paling Populer</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.chip}>
+          <Text style={styles.chipText}>Artikel</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.chip}>
+          <Text style={styles.chipText}>Video</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.chip}>
+          <Text style={styles.chipText}>Berita</Text>
+        </TouchableOpacity>
+      </ScrollView>
+
+        <View style={styles.postContainer}>
+            <TouchableOpacity>
+                <Text style={styles.berikut}>Berikut</Text>
+            </TouchableOpacity>
+        </View>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.articleContainer}>
+        <View style={styles.articleCard}>
+          <FontAwesome name="heart" size={20} color="#ccc" style={styles.heartIcon} />
+          <View style={styles.articleContent}>
+            <Text style={styles.articleTitle}>Berita, Lokasi</Text>
+            <Text style={styles.articleLocation}>Kab, Lokasi</Text>
+            <Text style={styles.articleStats}>4.8K</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    paddingTop: 10,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  greeting: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  profileImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 20,
+    backgroundColor: '#ECE6F0',
+    borderRadius: 30,
+    paddingHorizontal: 10,
+  },
+
+shadowSearch : {
+    shadowColor : "#000000",
+    shadowOpacity : 0.2,
+    shadowRadius : 4,
+    shadowOffset : {width: 0, height: 4}
+},
+
+  searchInput: {
+    flex: 1,
+    padding: 10,
+    fontSize: 16,
+  },
+  searchIcon: {
+    marginLeft: 10,
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+
+  postContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  viewAll: {
+    color: '#888888',
+    fontSize: 16,
+  },
+
+  berikut: {
+    color: '#1E1E1E',
+    fontSize: 16,
+    justifyContent: "flex-end"
+  },
+
+  chipContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    marginVertical: 10,
+  },
+  chip: {
+    backgroundColor: '#eae6fd',
+    borderRadius: 20,
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    marginRight: 10,
+    height: 35
+  },
+  chipText: {
+    color: '#6c4bf4',
+  },
+  articleContainer: {
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    marginVertical: 10,
+  },
+  articleCard: {
+    width: 250,
+    height: 350,
+    backgroundColor: '#D9D9D9',
+    borderRadius: 30,
+    marginRight: 15,
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  heartIcon: {
+    alignSelf: 'flex-end',
+    padding : 10,
+    color : "red",
+  },
+  articleContent: {
+    marginTop: 20,
+    padding : 10
+  },
+  articleTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  articleLocation: {
+    color: '#777',
+  },
+  articleStats: {
+    fontSize: 14,
+    color: '#333',
+  },
+  bottomNav: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#6c4bf4',
+    paddingVertical: 10,
+  },
+})
+
+export default Home
