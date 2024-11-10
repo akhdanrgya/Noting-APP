cfb75e69dbcf588a9642bde9e1638d76f5258e5d
akhdanrgya <akhdan.anargya@gmail.com>
2024-11-10 14:40:54 +0700

    update

--- hooks/firebaseConfig.ts
+++ hooks/firebaseConfig.ts
@@ -4,21 +4,21 @@ import { getFirestore } from 'firebase/firestore'
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    // apiKey: process.env.FIREBASE_API_KEY,
+    // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    // projectId: process.env.FIREBASE_PROJECT_ID,
+    // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.FIREBASE_APP_ID,
+    // measurementId: process.env.FIREBASE_MEASUREMENT_ID
 
-  // apiKey: "AIzaSyCCMCBL0r3iu6BEJDWgOkq9geND4M6C-AE",
-  // authDomain: "gokiltech.firebaseapp.com",
-  // projectId: "gokiltech",
-  // storageBucket: "gokiltech.firebasestorage.app",
-  // messagingSenderId: "941836526215",
-  // appId: "1:941836526215:web:d67cee168e92f505e14e22",
-  // measurementId: "G-76XRB2WLT8"
+  apiKey: "AIzaSyCCMCBL0r3iu6BEJDWgOkq9geND4M6C-AE",
+  authDomain: "gokiltech.firebaseapp.com",
+  projectId: "gokiltech",
+  storageBucket: "gokiltech.firebasestorage.app",
+  messagingSenderId: "941836526215",
+  appId: "1:941836526215:web:d67cee168e92f505e14e22",
+  measurementId: "G-76XRB2WLT8"
 }
 
 // Initialize Firebase App
