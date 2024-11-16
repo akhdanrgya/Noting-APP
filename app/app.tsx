1f00828600300763a8a5f88f72f75f2a0352dd5e
akhdanrgya <akhdan.anargya@gmail.com>
2024-11-16 14:45:36 +0700

    Update dotenv bug

---
+++ app/app.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const app = () => {
+    return (
+        <View>
+            Anjay
+        </View>
+    )
+}
+
+export default app;
